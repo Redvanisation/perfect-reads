fa53acf8e66753dd98fa2da95bd33850bfd040c4
Radouane Khiri <redvanisation@gmail.com>
2022-02-20 22:35:10 -0500

    Add MongoDB cluster link

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://root:root@cluster0.81nem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'), UsersModule],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@cluster0.91iqf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
