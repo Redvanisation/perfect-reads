4b696bb8bcb922b902073750d342078436cf4a78
Radouane Khiri <redvanisation@gmail.com>
2022-10-01 22:41:14 -0400

    Show errors on login screen

--- apps/mobile/src/providers/AuthProvider.tsx
+++ apps/mobile/src/providers/AuthProvider.tsx
@@ -6,59 +6,67 @@ import { useDispatch } from 'react-redux';
 import { setUser, clearUser } from '../redux/slices/user/userSlice';
 
 type Props = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 export const AuthContext = createContext<AuthContextData>(null);
 
 export default function AuthProvider({ children }: Props) {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const [state, setState] = useState<State>({
-    isLoggedIn: false,
-    isLoading: false,
-    error: null,
-  });
-
-  const login = async (userData: UserLoginData) => {
-    setState({ ...state, isLoading: true });
-    
-    const response = await axios.post(`${SERVER_URL}/auth/login`, userData);
-    
-    if (response.status === 200 || response.status === 201) {
-      dispatch(setUser(response.data));
-      setState({
-        isLoggedIn: true,
-        isLoading: false,
-        error: null,
-      });
-    } else {
-      setState({
+    const [state, setState] = useState<State>({
         isLoggedIn: false,
         isLoading: false,
-        error: 'Login failed',
-      });
-    }
-  };
-
-  const logout = () => {
-    dispatch(clearUser());
-    setState({
-      isLoggedIn: false,
-      isLoading: false,
-      error: '',
+        error: null,
     });
-  };
 
-  return (
-    <AuthContext.Provider
-      value={{
-        state,
-        login,
-        logout,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  )
+    const login = async (userData: UserLoginData) => {
+        setState({ ...state, isLoading: true });
+        try {
+            const response = await axios.post(`${SERVER_URL}/auth/login`, userData);
+
+            if (response.status === 200 || response.status === 201) {
+                dispatch(setUser(response.data));
+                setState({
+                    isLoggedIn: true,
+                    isLoading: false,
+                    error: null,
+                });
+            } else {
+                setState({
+                    isLoggedIn: false,
+                    isLoading: false,
+                    error: 'Login failed',
+                });
+            }
+        } catch (error) {
+            console.log('ERR', error);
+            setState({
+                isLoggedIn: false,
+                isLoading: false,
+                error: error as string,
+            });
+        }
+    };
+
+    const logout = () => {
+        dispatch(clearUser());
+        setState({
+            isLoggedIn: false,
+            isLoading: false,
+            error: '',
+        });
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                state,
+                login,
+                logout,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 }
